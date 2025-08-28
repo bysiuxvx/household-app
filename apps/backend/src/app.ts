604ce4d502faf4bfaca1f0089f6864445bf15686
Patryk B <byszek.patryk@gmail.com>
2025-08-28 22:24:22 +0200

    [BE] rm unnecessary route (list route)

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -3,15 +3,18 @@ import cors from 'cors'
 import express, { Express } from 'express'
 
 import { householdRouter } from './routes/householdRoutes'
+import { listItemRouter } from './routes/listItemRoutes'
 
 const app: Express = express()
 
-app.use(cors());
+app.use(cors())
 
 app.use(express.json())
 
 app.use('/api', clerkMiddleware())
+
 app.use('/api/households', householdRouter)
+app.use('/api', listItemRouter)
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World!' })
