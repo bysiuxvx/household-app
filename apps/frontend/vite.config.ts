126466879095512db68334f3ad1dbc6bf02905f8
Patryk B <byszek.patryk@gmail.com>
2025-08-29 21:53:14 +0200

    [FE] build fix

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,10 +1,13 @@
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    // Enable React Fast Refresh
+    tsDecorators: true,
+  })],
   resolve: {
     alias: {
       '@household/shared': path.resolve(__dirname, '../../packages/shared/src'),
