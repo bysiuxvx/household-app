3275f6513f2d8da5909b1122199278523360cd52
Patryk B <byszek.patryk@gmail.com>
2025-08-29 15:25:35 +0200

    [BE] use verification route

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -4,6 +4,7 @@ import express, { Express } from 'express'
 
 import { householdRouter } from './routes/householdRoutes'
 import { listItemRouter } from './routes/listItemRoutes'
+import { verificationRouter } from './routes/verificationRoute'
 
 const app: Express = express()
 
@@ -15,6 +16,7 @@ app.use('/api', clerkMiddleware())
 
 app.use('/api/households', householdRouter)
 app.use('/api', listItemRouter)
+app.use('/api/verification', verificationRouter)
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World!' })
