aa09febfe34e3a7f9dcbaba5934b005a53d38ecc
Patryk B <byszek.patryk@gmail.com>
2025-09-05 21:52:47 +0200

    [W, BE] Fix server port and AWS App Runner configuration

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,7 +1,8 @@
 import app from './app'
 
-const PORT = process.env.PORT || 3000
+const PORT: string = process.env.PORT || '3000'
+const HOST: string = process.env.HOST || '0.0.0.0'
 
-app.listen(PORT, () => {
-  process.stdout.write(`Server is running at http://localhost:${PORT}\n`)
+app.listen(parseInt(PORT, 10), HOST, () => {
+  process.stdout.write(`Server is running at http://${HOST}:${PORT}\n`)
 })
